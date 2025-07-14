130133aa7234a2aaffd49bb6813a0ec98810e097
Guilherme Alves Profirio <iamalves.jpg@gmail.com>
2025-07-14 10:18:26 -0300

    feat: boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { Elysia } from "elysia";
 import { authController } from "./modules/auth";
+import cors from "@elysiajs/cors";
+import { userController } from "./modules/user";
+import { uploadController } from "./modules/upload";
+import { notificationsController } from "./modules/notifications";
 
 const app = 
   new Elysia()
+  .use(cors({
+      origin: "http://localhost:5173", // Your frontend URL
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization"],
+  }))
   .use(authController)
+  .use(userController)
+  .use(uploadController)
+  .use(notificationsController)
   .get("/health", () => "Healthy")
   .listen(3000);
 
